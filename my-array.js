9ad54b5b6ed9287291fcd0e673dfce4415c8fa48
Thenateraider <Ekaluck.musik@gmail.com>
2026-07-02 13:38:27 +0700

    Import DB of Menu Recipe in my-object.js in Project and integrated in index.html

---
+++ my-array.js
@@ -0,0 +1,11 @@
+let food = [
+    "Pad Thai",
+    "Bratwurst",
+    "Tom Yum Goong",
+    "Schnitzel",
+    "Green Curry",
+    "Brezel",
+    "Som Tum",
+    "Schwarzwälder Kirschtorte"
+];
+console.log(food[3]);
---
+++ my-object.js
@@ -0,0 +1,68 @@
+// ผัดไทย, Bratwurst, ต้มยำกุ้ง, Schnitzel, แกงเขียวหวาน, Brezel, ส้มตำ, Schwarzwälder Kirschtorte
+let foodDatabase = [
+    {
+        id: 1,
+        name: "Pad Thai",
+        cuisine: "Thai",
+        origin: "Thailand",
+        vegetarian: false,
+        image: "https://upload.wikimedia.org/wikipedia/commons/e/ed/Pad_Thai.JPG"
+    },
+    {
+        id: 2,
+        name: "Bratwurst",
+        cuisine: "German",
+        origin: "Germany",
+        vegetarian: false,
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/98/Bratwurst_%288352858827%29.jpg"
+    },
+    {
+        id: 3,
+        name: "Tom Yum Goong",
+        cuisine: "Thai",
+        origin: "Thailand",
+        vegetarian: false,
+        image: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Tom_yum_goong-01.jpg"
+    },
+    {
+        id: 4,
+        name: "Schnitzel",
+        cuisine: "German",
+        origin: "Germany",
+        vegetarian: false,
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/40/Wiener_Schnitzel.jpg"
+    },
+    {
+        id: 5,
+        name: "Green Curry",
+        cuisine: "Thai",
+        origin: "Thailand",
+        vegetarian: false,
+        image: "https://upload.wikimedia.org/wikipedia/commons/e/e5/Thai_green_chicken_curry_and_roti.jpg"
+    },
+    {
+        id: 6,
+        name: "Brezel",
+        cuisine: "German",
+        origin: "Germany",
+        vegetarian: true,
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Breze.jpg"
+    },
+    {
+        id: 7,
+        name: "Som Tum",
+        cuisine: "Thai",
+        origin: "Thailand",
+        vegetarian: true,
+        image: "https://upload.wikimedia.org/wikipedia/commons/0/0a/Som_tam_thai.JPG"
+    },
+    {
+        id: 8,
+        name: "Schwarzwälder Kirschtorte",
+        cuisine: "German",
+        origin: "Germany",
+        vegetarian: true,
+        image: "https://upload.wikimedia.org/wikipedia/commons/b/b0/Schwarzw%C3%A4lder_Kirschtorte.jpg"
+    }
+];
+console.log(foodDatabase[7].name);
